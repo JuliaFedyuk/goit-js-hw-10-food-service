de26b86c016a697162234b6fac8fa28fbfdf060e
Julia Fedyuk <fedyuk.julia@gmail.com>
2021-01-19 17:17:26 +0200

    Добавляет д/з

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import itemsTemplate from './menu.hbs';
+import items from './menu.json';
+import styles from './styles.css';
+
+// console.log(items);
+
+const markup = itemsTemplate(items);
+
+// console.log(markup);
+
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('afterbegin', markup);
+
+//  ==========================changeTheme===================
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+bodyRef.classList.add('light-theme');
+
+function themeSwitcher(event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove('light-theme');
+    bodyRef.classList.add('dark-theme');
+    localStorage.setItem('Theme', 'dark-theme');
+  } else {
+    bodyRef.classList.remove('dark-theme');
+    bodyRef.classList.add('light-theme');
+    localStorage.setItem('Theme', 'light-theme');
+  }
+}
+
+checkboxRef.addEventListener('change', themeSwitcher);
+
+const getTheme = localStorage.getItem('Theme');
+if (getTheme === 'dark-theme') {
+  checkboxRef.checked = true;
+  bodyRef.classList.add('dark-theme');
+}
